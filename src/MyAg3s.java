6b290ce6bc21a6012b30594a368465bd8bb331c2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-14 13:21:23 -0700

    Banna4Eva, MyAges, X Geeks All done

---
+++ src/MyAg3s.java
@@ -0,0 +1,8 @@
+
+public class MyAg3s {
+public static void main(String[] args) {
+	for (int i = 0; i < 12; i++) {
+		System.out.println(0 + 1);
+	}
+}
+}
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -1,9 +1,6 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-
-
 import javax.swing.JOptionPane;
 
 /*
@@ -12,14 +9,38 @@ import javax.swing.JOptionPane;
 * your program should say "June's superpower is writing recipes".
 */
 public class XGeeks {
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		String Kevin = "Invisible";
+		String Diego = "Super Lucky";
+		String Yen = "Levatation";
+		String Tra = "Super Tall";
+		String Daniel = "Super Strong";
 
-		// 1. Save the superpower for each person in a variable.
+		String name = JOptionPane.showInputDialog("Enter a name of someone at your table");
 
-		// 2. Ask the user to enter a name. Store their answer in a variable.
+		if (name.equals("Kevin")) {
+			JOptionPane.showMessageDialog(null, Kevin);
+		}
+		if (name.equals("Diego")) {
+			JOptionPane.showMessageDialog(null, Diego);
+		}
 
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		if (name.equals("Yen")) {
+			JOptionPane.showMessageDialog(null, Yen);
+		}
+		
+		if (name.equals("Tra")) {
+				JOptionPane.showMessageDialog(null, Tra);
+		}
+		
+		if (name.equals("Daniel")) {
+		JOptionPane.showMessageDialog(null, Daniel);
+		
+		}
 	}
 }
+// 1. Save the superpower for each person in a variable.
 
+// 2. Ask the user to enter a name. Store their answer in a variable.
+
+// 3. Show the superpower in a pop-up, depending on the name entered.
